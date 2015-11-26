b1d19cfc2f8e382136d69c8875d5c112d104d4cd
alexoviedo <alexoviedo999@gmail.com>
2015-11-26 11:59:54 -0500

    list item editing now works.

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,25 +1,40 @@
 var React = require('react');
+var Firebase = require('firebase');
+var rootUrl = 'https://react-firebase-todos.firebaseio.com/';
 
 module.exports = React.createClass({
 	getInitialState: function () {
 		return {
-			text: this.props.item.text
+			text: this.props.item.text,
+			done: this.props.item.done
 		}
+	},
+	componentWillMount: function () {
+		this.fb = new Firebase(rootUrl + 'items/' + this.props.item.key)
 	},
 	render: function () {
 		return <div className="input-group">
-		<span className="input-group-addon">
-		<input type="checkbox" />
-		</span>
-		<input type="text"
-		className="form-control"
-		value={this.state.text}
-		/>
-		<span className="input-group-btn">
-		<button className="btn btn-default">
-		Delete
-		</button>
-		</span>
+			<span className="input-group-addon">
+				<input
+					type="checkbox"
+					checked={this.state.done}
+					onChange={this.handleDoneChange}
+					/>
+			</span>
+			<input type="text"
+				className="form-control"
+				value={this.state.text}
+			/>
+			<span className="input-group-btn">
+				<button className="btn btn-default">
+					Delete
+				</button>
+			</span>
 		</div>
+	},
+	handleDoneChange: function (event) {
+		var update = {done:event.target.checked};
+		this.setState(update);
+		this.fb.update(update);
 	}
 })
